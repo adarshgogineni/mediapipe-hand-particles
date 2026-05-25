220e35a5b0d784bb34ed5d3035c42fbebc5b4bee
Adarsh Gogineni <adarshgogineni@gmail.com>
2026-05-25 16:03:24 -0400

    feat: MediaPipe Hands landmark detection

--- sketch.js
+++ sketch.js
@@ -1,10 +1,40 @@
 const PARTICLE_COUNT = 3000;
 let particles = [];
 let capture;
+let handLandmarks = []; // updated each frame by MediaPipe
+
+function setupMediaPipe(videoEl) {
+  const hands = new Hands({
+    locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
+  });
+
+  hands.setOptions({
+    maxNumHands: 2,
+    modelComplexity: 1,
+    minDetectionConfidence: 0.7,
+    minTrackingConfidence: 0.5
+  });
+
+  hands.onResults((results) => {
+    handLandmarks = results.multiHandLandmarks || [];
+  });
+
+  const camera = new Camera(videoEl, {
+    onFrame: async () => {
+      await hands.send({ image: videoEl });
+    },
+    width: 640,
+    height: 480
+  });
+  camera.start();
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  capture = createCapture(VIDEO);
+  // Pass video element to MediaPipe once capture is ready
+  capture = createCapture(VIDEO, () => {
+    setupMediaPipe(capture.elt);
+  });
   capture.size(width, height);
   capture.hide();
 
@@ -14,14 +44,12 @@ function setup() {
 }
 
 function draw() {
-  // Webcam background (mirrored)
   push();
   translate(width, 0);
   scale(-1, 1);
   image(capture, 0, 0, width, height);
   pop();
 
-  // Semi-transparent overlay creates particle trails
   noStroke();
   fill(0, 0, 0, 50);
   rect(0, 0, width, height);
