e29bc1161169d69747b2709c9ec1eb8663cb8536
Adarsh Gogineni <adarshgogineni@gmail.com>
2026-05-25 15:43:48 -0400

    feat: mirrored webcam background

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
+let capture;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  capture = createCapture(VIDEO);
+  capture.size(width, height);
+  capture.hide();
 }
 
 function draw() {
-  background(0);
+  // Mirror horizontally for selfie orientation
+  push();
+  translate(width, 0);
+  scale(-1, 1);
+  image(capture, 0, 0, width, height);
+  pop();
 }
 
 function windowResized() {
